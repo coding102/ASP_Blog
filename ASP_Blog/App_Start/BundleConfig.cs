fadc287cba1f855899cedb68b79629f508ab0802
mark <mercercountynj@gmail.com>
2016-03-15 18:43:35 -0400

    bootstrap import

---
+++ ASP_Blog/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Optimization;
+
+namespace ASP_Blog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+        }
+    }
+}
