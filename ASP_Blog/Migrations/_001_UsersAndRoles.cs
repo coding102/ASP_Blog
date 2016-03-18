d0f7cd730d44ef44296c6b59c60618f4d64093a0
mark <mercercountynj@gmail.com>
2016-03-17 20:54:23 -0500

    fluentmigrator and mysql

---
+++ ASP_Blog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentMigrator;
+
+namespace ASP_Blog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(120)
+                .WithColumn("email").AsString("256")
+                .WithColumn("password_hash").AsString("128");
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
